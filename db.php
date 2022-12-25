947c5c01f71ca5322c4009d05fc5c26bc90e3eb0
Adarsh Telkar <adarshtelkar002@gmail.com>
2022-12-25 18:08:13 +0530

    message delete

--- db.php
+++ db.php
@@ -3,5 +3,6 @@ $mysql_hostname = "localhost";
 $mysql_user = "root";
 $mysql_password = "";
 $mysql_database = "profile";
+//configuration 
 $con = mysqli_connect($mysql_hostname, $mysql_user, $mysql_password, $mysql_database);
 ?>
